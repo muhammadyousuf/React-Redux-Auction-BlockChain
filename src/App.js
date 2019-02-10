d1df773000c47bcaa4ee23b88a7fdbd2ed66618b
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-10 11:52:06 +0500

    add header and footer component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import List from './Components/List/Screen';
 import { Provider } from 'react-redux';
 import store from './Store/store';
 import DemoCarousel from './Components/Slider/Slider';
+import Header from './Components/Header/Header';
+
 
 import './App.css';
+import Footer from './Components/Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store} >
+      <Header />
         <BrowserRouter>
           <Switch>
             <Route exact component={List} path="/" />
@@ -23,6 +27,7 @@ class App extends Component {
           <AddProduct />
           <List />
         </div> */}
+        <Footer />
       </Provider>
     );
   }
