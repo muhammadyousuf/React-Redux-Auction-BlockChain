b1c38c7d860d017ae91475496d9da76ed61e8381
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-10 14:18:18 +0500

    nav Bar add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import './Header.css';
 
- 
+
 class Header extends Component {
+    constructor(props){
+    super(props);
+    this.state = {
+        token:''
+    }
+
+    }
+    componentWillMount(){
+        let key;
+        key = localStorage.getItem('token');
+        this.setState({token:key}, ()=>{
+            console.log(this.state.token)
+        })
+    }
     render() {
         return (
-            <div  >
-            <h1>Header</h1>
-            </div>
+            <div  className="container-fuild HeaderColor" >
+                <Navbar >
+                    <Nav className="mr-auto">
+                    {this.state.token === '' || this.state.token === null ?
+                        <span>
+                            
+                            <Navbar.Brand  className="headerTitle" >Login</Navbar.Brand>
+                            <Navbar.Brand  className="headerTitle" >About</Navbar.Brand>
+                        </span>:
+                        <span>
+                            <Navbar.Brand  className="headerTitle" >Home</Navbar.Brand>
+                            <Navbar.Brand  className="headerTitle" >Dashboard</Navbar.Brand>
+                            <Navbar.Brand  className="headerTitle" >Add Auction</Navbar.Brand>
+                            <Navbar.Brand  className="headerTitle" >My Auction</Navbar.Brand>
+                            <Navbar.Brand  className="headerTitle" >History</Navbar.Brand>
+                        </span>
+                    }
+                    </Nav>
+                </Navbar>  </div>
+
         );
     }
 }
