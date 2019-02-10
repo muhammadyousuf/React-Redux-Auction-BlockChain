55a52e9623bc6fcef8584b79e0188423411db9be
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-10 15:35:42 +0500

    responseive slider

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -20,6 +20,7 @@ class Header extends Component {
     }
     render() {
         return (
+            
             <div  className="container-fuild HeaderColor" >
                 <Navbar >
                     <Nav className="mr-auto">
--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,39 +1,41 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
- 
+
 class DemoCarousel extends Component {
     render() {
         return (
-            <div className="row" style={{backgroundColor:'green'}} >
-            <div className="col-md-6 col-md-offset-2 col-xs-6 col-xs-offset-4 col-sm-6 col-sm-offset-4 col-lg-6 col-lg-offset-4 col-xl-6 col-xl-offset-4" style={{width:550, height:300, marginTop:'5%'}}>
-            <Carousel  >
-            <div >
-                    <img src={require('../../images/car.jpg')} alt="" />
-                    <p className="legend">Legend 1</p>
+            <div className="row"  >
+                <div className="col-md-6 col-md-offset-3 col-xs-6 col-xs-offset-3 col-sm-6 col-sm-offset-3 col-lg-6 col-lg-offset-3 col-xl-6 col-xl-offset-3" style={{ marginTop: '5%' }}>
+                    <Carousel  >
+                        <div>
+                            <img src={require('../../images/house.jpg')} alt="" />
+                            <p className="legend">House</p>
+                        </div>
+                        <div >
+                            <img src={require('../../images/car.jpg')} alt="" />
+                            <p className="legend">Car</p>
+                        </div>
+                        <div >
+                            <img src={require('../../images/bike.jpg')} alt="" />
+                            <p className="legend">Bike</p>
+                        </div>
+                        <div >
+                            <img src={require('../../images/mobile.jpg')} alt="" />
+                            <p className="legend">Mobile</p>
+                        </div>
+                        <div>
+                            <img src={require('../../images/laptop.jpg')} alt="" />
+                            <p className="legend">Laptop</p>
+                        </div>
+                        <div>
+                            <img src={require('../../images/computer.jpg')} alt="" />
+                            <p className="legend">Computer</p>
+                        </div>
+
+                    </Carousel>
                 </div>
-                <div >
-                    <img src={require('../../images/bike.jpg')} alt="" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div >
-                    <img src={require('../../images/mobile.jpg')} alt="" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src={require('../../images/laptop.jpg')} alt=""  />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src={require('../../images/computer.jpg')} alt="" />
-                    <p className="legend">Legend 3</p>
-                </div>
-                <div>
-                    <img src={require('../../images/white.jpg')} alt="" />
-                </div>
-            </Carousel>
-            </div>
-           
+
             </div>
         );
     }
