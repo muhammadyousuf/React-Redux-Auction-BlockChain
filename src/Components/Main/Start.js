dd7cab32782c0ef9c87881c0817ff19f6b7f69ab
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-12 19:41:28 +0500

    main pages responsive

--- src/Components/Main/Start.js
+++ src/Components/Main/Start.js
@@ -10,12 +10,12 @@ class Start extends Component {
                 <h1 className="startHeader" >Auctionity</h1>
                 <h3 className="startSubHeader" >The world's largest blockchain auction house for NTFs</h3>
                 <div className="row topStyle justify-content-center"  >
-                    <div className="col-md-6 paperDiv" >
+                    <div className="col-md-5 col-sm-5 col-xs-6 paperDiv" >
                         <img src="https://www.auctionity.com/wp-content/uploads/2018/09/Icone-Etoile.png" alt="" className="scale-with-grid imgCenter" />
                         <h1 className="divHeader" >Creating auctions and bidding</h1>
                         <p className="divParagrph" >Auctionity Saleroom</p>
                     </div>
-                    <div className="col-md-6 paperDivTwo" >
+                    <div className="col-md-5 col-sm-5 col-xs-6 paperDivTwo" >
                         <img src="https://www.auctionity.com/wp-content/uploads/2018/09/Icone-cour.png" alt="" className="scale-with-grid imgCenter" />
                         <h1 className="divHeader">Community Rewards</h1>
                         <p className="divParagrph">Auctionity Club</p>
