78fe2c448b5c091060c711b0dfa7e93d830859c3
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-15 22:43:04 +0500

    create dashboard and my auction

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Dasboard.css';
+
+
+class Dashboard extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+        
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+
+            </div>
+        );
+    }
+}
+export default Dashboard;
---
+++ src/Components/MyAuction/MyAuction.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './MyAuction.css';
+
+
+class MyAuction extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+        
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+
+            </div>
+        );
+    }
+}
+export default MyAuction;
