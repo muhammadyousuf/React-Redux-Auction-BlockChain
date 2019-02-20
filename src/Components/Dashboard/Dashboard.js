7ecc6065b2b9dcd056a0cd11e3fb72ca49eab24e
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-20 12:19:08 +0500

    dashbaord design completed

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -3,9 +3,9 @@ import './Dashboard.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import car from '../../images/car.jpg'
-// import bike from '../../images/bike.jpg'
-// import computer from '../../images/computer.jpg'
-// import house from '../../images/house.jpg'
+import bike from '../../images/bike.jpg'
+import computer from '../../images/computer.jpg'
+import house from '../../images/house.jpg'
 
 
 
@@ -27,25 +27,117 @@ class Dashboard extends Component {
                         <div className="col-md-4 col-sm-12 col-xs-12 " >
                             <div className="listHead" >
                                 <p className="headPara">Car</p>
-                                <p className="headDate">17/02/2019 01:00 AM</p>
+                                <p className="headDate">200000$</p>
                             </div>
-                            <div>
-                                <img src={`${car}`} alt="" className="images" />
-                              
+                            <div className="containers">
+                                <img src={`${car}`} alt="Avatar" className="image"   />
+                                    <div className="middle">
+                                        <div className="text">Detail</div>
+                                    </div>
+</div>
+
+                                <div className="listFoot " >
+                                    <p className="footendDate">Start Date:</p>
+                                    <p className="footPrice" >17/02/2019 01:00 AM</p>
+
+                                </div>
                             </div>
+                            <div className="col-md-4 col-sm-12 col-xs-12 " >
+                            <div className="listHead" >
+                                <p className="headPara">Bike</p>
+                                <p className="headDate">100000$</p>
+                            </div>
+                            <div className="containers">
+                                <img src={`${bike}`} alt="Avatar" className="image"   />
+                                    <div className="middle">
+                                        <div className="text">Detail</div>
+                                    </div>
+</div>
 
-                            <div className="listFoot " >
-                                <p className="footendDate">20/02/2019 06:00 PM</p>
-                                <p className="footPrice" >200000$</p>
+                                <div className="listFoot " >
+                                    <p className="footendDate">Start Date:</p>
+                                    <p className="footPrice" >25/02/2019 05:00 AM</p>
 
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-12 col-xs-12 " >
+                            <div className="listHead" >
+                                <p className="headPara">House</p>
+                                <p className="headDate">500000$</p>
+                            </div>
+                            <div className="containers">
+                                <img src={`${house}`} alt="Avatar" className="image"   />
+                                    <div className="middle">
+                                        <div className="text">Detail</div>
+                                    </div>
+</div>
+
+                                <div className="listFoot " >
+                                    <p className="footendDate">Start Date:</p>
+                                    <p className="footPrice" >02/03/2019 03:00 PM</p>
+
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-12 col-xs-12 " >
+                            <div className="listHead" >
+                                <p className="headPara">Computer</p>
+                                <p className="headDate">5000$</p>
+                            </div>
+                            <div className="containers">
+                                <img src={`${computer}`} alt="Avatar" className="image"   />
+                                    <div className="middle">
+                                        <div className="text">Detail</div>
+                                    </div>
+</div>
+
+                                <div className="listFoot " >
+                                    <p className="footendDate">Start Date:</p>
+                                    <p className="footPrice" >08/03/2019 06:00 PM</p>
+
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-12 col-xs-12 " >
+                            <div className="listHead" >
+                                <p className="headPara">Car</p>
+                                <p className="headDate">60000$</p>
+                            </div>
+                            <div className="containers">
+                                <img src={`${car}`} alt="Avatar" className="image"   />
+                                    <div className="middle">
+                                        <div className="text">Detail</div>
+                                    </div>
+</div>
+
+                                <div className="listFoot " >
+                                    <p className="footendDate">Start Date:</p>
+                                    <p className="footPrice" >15/03/2019 05:00 AM</p>
+
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-12 col-xs-12 " >
+                            <div className="listHead" >
+                                <p className="headPara">Hoouse</p>
+                                <p className="headDate">700000$</p>
+                            </div>
+                            <div className="containers">
+                                <img src={`${house}`} alt="Avatar" className="image"   />
+                                    <div className="middle">
+                                        <div className="text">Detail</div>
+                                    </div>
+</div>
+
+                                <div className="listFoot " >
+                                    <p className="footendDate">Start Date:</p>
+                                    <p className="footPrice" >22/03/2019 08:00 AM</p>
+
+                                </div>
                             </div>
                         </div>
-                    </div>
 
+                    </div>
+                    <Footer />
                 </div>
-                <Footer />
-            </div>
-        );
-    }
-}
+                );
+            }
+        }
 export default Dashboard;
